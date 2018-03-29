08f34b6cb770cf103ac722028266e37dbcb90a66
hansmrtn <hansmrtn@gmail.com>
2018-03-29 09:02:07 -0700

    Add the batch file to run on windows with a single click, and of course, the python script itself.

---
+++ run.py
@@ -0,0 +1,94 @@
+'''
+Hans Martin
+University of Washington
+APL
+
+Last Edited: March 26, 2018
+'''
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt                                   
+
+'''
+CONSTANTS
+'''
+k = 1.38E-23
+T = 10*1.16E4
+area = 1.749E-5
+e = 1.602E-19
+mass = 6.67E-26
+crctn = 0.004
+c = (0.6*e*area*np.sqrt(k*T/mass))
+N = 500
+
+'''
+RMS, mean, density functions
+'''
+
+def rms(v):
+    rms = crctn*np.sqrt(np.convolve(np.square(v), np.ones((N,))/N, mode='same'))
+    return(rms)
+
+def mean(rms): 
+    for i in range(0,len(RMS)):
+        m =+ RMS[i]/len(RMS)
+    return(m)
+
+def dens(mean):
+    d = np.divide(mean, c, out=np.zeros_like(mean), where=c!=0)
+    return(d)
+
+
+if (os.getcwd != 'data'): 
+    os.chdir('data')
+
+dirpath = {}
+for f in os.listdir():
+    dirpath[f] = []
+
+for f, data in dirpath.items():
+    for t in os.listdir(f):
+        data.append(t)
+
+vt = []
+for f, data in dirpath.items():
+    df = []
+    for i in data:
+        df.append(np.ndfromtxt(f+'/'+i, delimiter='\t'))
+    vt.append(df)
+
+'''
+vt[folder][txt file][time, voltage]
+'''
+RMS = []
+for k in range(0,len(vt)):
+    for i in range(0,len(vt[k])):
+        RMS.append(rms(vt[k][i][:,1]))
+        t = vt[k][i][:,0]
+
+    plt.figure()
+    plt.ion()
+    for r in range(0,len(RMS)):
+        plt.plot(t,RMS[r])
+    plt.title(str(os.listdir()[k])+' RMS')
+    plt.xlabel('Seconds')
+
+
+    plt.figure()
+    plt.ion()
+    plt.plot(t,mean(RMS))
+    plt.title(str(os.listdir()[k])+' Average')
+    plt.xlabel('Seconds')
+
+
+    plt.figure()
+    plt.ion()
+    plt.plot(t,dens(mean(RMS)))
+    plt.title(str(os.listdir()[k])+' Density')
+    plt.xlabel('Seconds')
+    plt.ylabel('$m^-3$')
+
+
+plt.show(block=True)
+
